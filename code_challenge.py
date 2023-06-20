dcd2d4bc0996680ead7af56d343965af4e6838a8
Peter Todorov <peter.todorov@live.com>
2023-06-20 19:54:35 +0200

    Make distance minimization work

--- code_challenge.py
+++ code_challenge.py
@@ -3,6 +3,28 @@ from ortools.constraint_solver import pywrapcp
 import math
 import matplotlib.pyplot as plt
 import random
+import numpy as np
+
+
+def printSolution(manager, routing, solution):
+    print(f'Objective: {solution.ObjectiveValue()}')
+    vehicle_id = 0
+    total_distance = 0
+    index = routing.Start(vehicle_id)
+    plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
+    route_distance = 0
+    while not routing.IsEnd(index):
+        plan_output += ' {} -> '.format(manager.IndexToNode(index))
+        previous_index = index
+        index = solution.Value(routing.NextVar(index))
+        route_distance += routing.GetArcCostForVehicle(
+            previous_index, index, vehicle_id)
+    plan_output += '{}\n'.format(manager.IndexToNode(index))
+    plan_output += 'Distance of the route: {}m\n'.format(route_distance)
+    print(plan_output)
+    total_distance += route_distance
+    print('Total Distance of all routes: {}m'.format(total_distance))
+
 
 
 def solveDeliveryRouting(packages, distance_matrix, driver_working_hours):
@@ -17,26 +39,31 @@ def solveDeliveryRouting(packages, distance_matrix, driver_working_hours):
         1,
         0,  # Depot 0
     )
-    #print(manager.GetEndIndex(0))
     
     # Create the routing model
     routing = pywrapcp.RoutingModel(manager)
   
     # Define the distance callback
     def distanceCallback(from_index, to_index):
-        return distance_matrix[from_index][to_index]
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return distance_matrix[from_node][to_node]
 
     transit_callback_index = routing.RegisterTransitCallback(distanceCallback)
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
   
     # Define the maximum distance constraint for each package
     for i in range(len(packages)):
+        dimension_name = "Distance"
         routing.AddDimension(
             transit_callback_index,
             0,  # no slack
             max_delivery_distance[i],  # package max delivery distance
             False,
-            "Distance",
+            dimension_name,
         )
+        distance_dimension = routing.GetDimensionOrDie(dimension_name)
+        distance_dimension.SetGlobalSpanCostCoefficient(100)
   
         # Set the deadline constraint for each package
         deadline = int(delivery_deadlines[i] * 60)  # Convert deadlines to minutes
@@ -49,14 +76,14 @@ def solveDeliveryRouting(packages, distance_matrix, driver_working_hours):
         )
   
     # Set the lunch break constraint
-    lunch_break_duration = 60  # minutes
-    routing.AddDimension(
-        transit_callback_index,
-        lunch_break_duration,
-        driver_working_hours * 60,  # working hours in minutes
-        True,
-        "Working Hours",
-    )
+    #lunch_break_duration = 60  # minutes
+    #routing.AddDimension(
+    #    transit_callback_index,
+    #    lunch_break_duration,
+    #    driver_working_hours * 60,  # working hours in minutes
+    #    True,
+    #    "Working Hours",
+    #)
 
     # Set up search parameters
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
@@ -73,7 +100,8 @@ def solveDeliveryRouting(packages, distance_matrix, driver_working_hours):
         while not routing.IsEnd(index):
             route.append(locations[index])
             index = solution.Value(routing.NextVar(index))
-        #route.append(locations[index])
+        route.append(locations[0])
+        printSolution(manager, routing, solution)
         return route
   
     return None
@@ -84,15 +112,15 @@ def calculateDistanceMatrix(depot_coordinates, data):
     data = [depot_coordinates] + data
 
     # Euclidian distance
-    for i in range(len(distance_matrix)):
+    for i in range(len(data)):
         location_distances = []
-        for j in range(len(distance_matrix)):
+        for j in range(len(data)):
             location_distances.append(math.sqrt(
                 (data[i]["x"] - data[j]["x"])**2 +
                 (data[i]["y"] - data[j]["y"])**2
             ))
-        distance_matrix.append(location_distances)
-    return distance_matrix
+        distance_matrix.append(np.array(location_distances))
+    return np.array(distance_matrix)
 
 
 def plotSolution(depot_coordinates, packages, route):
@@ -114,7 +142,7 @@ def plotSolution(depot_coordinates, packages, route):
 
 
 def main():
-    number_of_packages = 10
+    number_of_packages = 100
 
     # Data
     driver_working_hours = 8
@@ -132,7 +160,7 @@ def main():
         packages.append({
             "location": i+1,
             "deadline": random.randint(10, 50),
-            "max_delivery_distance": random.randint(100, 500),
+            "max_delivery_distance": random.randint(1000, 10000),
             "x": x,
             "y": y,
         })
